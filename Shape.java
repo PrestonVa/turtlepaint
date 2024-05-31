bbe7e15f40a9ba3f72a3e96c8f25a8a23bf1984c
PrestonVa <166422855+PrestonVa@users.noreply.github.com>
2024-05-31 11:57:26 -0400

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,45 @@
+package com.pluralsight;
+
+import java.awt.Color;
+import java.awt.Point;
+
+public abstract class Shape {
+    // Create the variables.
+    private World world;
+    Color color;
+    double border;
+
+    // Create the constructor.
+    public Shape(World world) {
+        this.world = world;
+    }
+
+    // Create getters and setters.
+    public World getWorld() {
+        return world;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public double getBorder() {
+        return border;
+    }
+
+    public void setBorder(double border) {
+        this.border = border;
+    }
+
+    // Create abstract methods.
+    public abstract void setLocation(Point location);
+
+    public abstract void paint();
+
+    public abstract Point getLocation();
+}
+
